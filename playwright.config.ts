3a27557a9d43f912c3df8c87cb73514313c78bcb
J01-L0142 <komal.arya@jupitice.com>
2026-05-26 12:21:06 +0530

    headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       use: { 
         ...devices['Desktop Chrome'], 
         channel: 'chrome',
-        headless: false 
+        headless: true 
       },
     },
 
